6012d82b6b32ede7f5d4bb5566e1e315c9005915
ademosky <almir.ademoski@gmail.com>
2023-02-05 16:31:08 +0100

    update

--- src/test/java/testCases/ShoppingCartTestCases.java
+++ src/test/java/testCases/ShoppingCartTestCases.java
@@ -2,7 +2,7 @@ package testCases;
 
 import java.util.concurrent.TimeUnit;
 
-import org.testng.Assert;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
